6f5e75099d5a94a5c2f67179bc443841c1b2f65d
ahmedmohamed299 <ahmed_mohamed299@hotmail.com>
2020-01-11 22:49:45 +0200

    a

--- app/src/main/java/com/example/jumiaandroidx/EditAccount.java
+++ app/src/main/java/com/example/jumiaandroidx/EditAccount.java
@@ -1,27 +1,47 @@
 package com.example.jumiaandroidx;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.jumiaandroidx.Model.User;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.example.jumiaandroidx.SignUp.RESULT_LOAD_IMAGE;
 
 public class EditAccount extends AppCompatActivity {
 
+    private static final String TAG = "EditAccount";
     @BindView(R.id.back)
     ImageView back;
     @BindView(R.id.edit)
@@ -49,7 +69,12 @@ public class EditAccount extends AppCompatActivity {
     ConstraintLayout addPhoto;
     @BindView(R.id.Phone)
     EditText Phone;
-
+    @BindView(R.id.profilePic)
+    ImageView profilePic;
+    @BindView(R.id.addIcon)
+    ImageView addIcon;
+    Uri selectedImage;
+    Map<String, Object> addUser = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,17 +84,24 @@ public class EditAccount extends AppCompatActivity {
         getUserDetails();
 
 
-
         addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent,RESULT_LOAD_IMAGE);
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
 
 
                 Toast.makeText(EditAccount.this, "Photo added", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UploadData();
+
             }
         });
 
@@ -84,6 +116,69 @@ public class EditAccount extends AppCompatActivity {
         companyName.setText(User.currentUser.getCompanyName());
         address.setText(User.currentUser.getAddress());
         Phone.setText(User.currentUser.getPhone());
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+
+            selectedImage = data.getData();
+            profilePic.setImageURI(selectedImage);
+
+
+        } else {
+            Toast.makeText(this, "hii", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void UploadData() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        Date date = new Date();
+        StorageReference storageReference = storage.getReference();
+        final StorageReference ref = storageReference.child(User.Contract.STORAGE+"/" + "testPROFILE" + ".jpg");
+        Log.i(TAG, "ref: " + ref + "    selectedImage: " + selectedImage);
+        UploadTask uploadTask = ref.putFile(selectedImage);
+
+        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+
+
+                }
+                return ref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    addUser.put(User.Contract.FIELD_FIRST_NAME, firstName.getText().toString());
+                    addUser.put(User.Contract.FIELD_SECOND_NAME, lastName.getText().toString());
+                    addUser.put(User.Contract.FIELD_COMPANY_NAME, companyName.getText().toString());
+                    addUser.put(User.Contract.FIELD_EMAIL, email.getText().toString());
+                    addUser.put(User.Contract.FIELD_PASSWORD, password.getText().toString());
+                    addUser.put(User.Contract.FIELD_ADDRESS, address.getText().toString());
+                    addUser.put(User.Contract.FIELD_PHONE, Phone.getText().toString());
+                    addUser.put(User.Contract.FIELD_PHOTO, downloadUri.toString());
+                    db.collection(User.Contract.DOC)
+                            .document(User.currentUser.getReference().getId())
+                            .set(addUser, SetOptions.merge());
+
+                                    Intent intent =new Intent(EditAccount.this,Home.class);
+                                    startActivity(intent);
+
+
+                }
+
+            }
+        });
     }
 
 
